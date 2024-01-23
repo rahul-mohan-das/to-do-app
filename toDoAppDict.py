cf2171b006a52c322bbf00851dc712c9afd6d351
rahul-mohan-das <rl.4601@gmail.com>
2024-01-23 11:48:08 +0530

    dictionary-added

--- toDoAppDict.py
+++ toDoAppDict.py
@@ -1,19 +1,19 @@
 class ToDoList:
     def __init__(self):
-        self.tasks=[]
+        self.tasks=dict()
     def addTask(self,task):
-        self.tasks.append(task)
+        self.tasks[len(self.tasks) + 1]=task
         print("The task has been added successfully")
     def viewTasks(self):
         if not self.tasks:
             print('No tasks found. Add some tasks first.')
         else:        
-            for i in self.tasks:
-                print("{}.{}".format(self.tasks.index(i)+1,i))
+            for i,j in self.tasks.items():
+                print("{}.{}".format(i,j))
     def removeTask(self, index):
         if 1 <= index <= len(self.tasks):
-            removed_task = self.tasks.pop(index - 1)
-            print('Task "{}" removed successfully.'.format(removed_task))
+            removed_task = self.tasks.pop(index)
+            print('Task "{}" removed successfully.'.format(removed_task))                
         else:
             print('Invalid task index.')
                 
@@ -24,7 +24,6 @@ def main():
         print("2. View task")
         print("3. Remove task")
         print("4. Exit")
-        print("5. Backup")
     while True:
         displayOptions()
         choice=int(input("Enter your choice 1/2/3/4\n"))
